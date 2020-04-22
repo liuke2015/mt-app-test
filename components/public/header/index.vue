864c13808de742106d41de4fe66697ce3d95bb32
liuke2015 <376060613@qq.com>
2020-04-22 18:02:10 +0800

    feat:新增header公共组件

---
+++ components/public/header/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="index">
+      <el-row :gutter="15">
+        <el-col :span="6">
+          <geo/>
+        </el-col>
+        <el-col :span="6">
+          <user/>
+        </el-col>
+        <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+      </el-row>
+    </div>
+</template>
+
+<script>
+    import geo from "./geo.vue"
+    import user from "./user.vue"
+    export default {
+        name: "index",
+        msg: "",
+        data() {
+            return {}
+        },
+       components:{
+         geo,
+         user
+       }
+    }
+</script>
+
+<style lang="scss">
+   @import "@/assets/css/public/layout.scss";
+</style>
